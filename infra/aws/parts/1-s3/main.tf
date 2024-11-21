c7096dabc62d31b3947353b897554efcf7cb047f
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-22 01:20:39 +0900

    infra : s3 微修正

--- infra/aws/parts/1-s3/main.tf
+++ infra/aws/parts/1-s3/main.tf
@@ -31,8 +31,5 @@ module "s3_bucket" {
   attach_policy = each.value.attach_policy
   policy        = each.value.attach_policy ? each.value.policy : null
   cors_rule = each.value.cors_rule
-  logging = each.value.logging != null ? {
-    target_bucket = each.value.logging.target_bucket
-    target_prefix = each.value.logging.target_prefix
-  } : {}
+  logging = {}
 }
