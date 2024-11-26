b32ea44eb2a42598d7c7684d9e001fca5f0618c4
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-26 16:16:55 +0900

    infra : aws cognitoを構築

---
+++ infra/aws/parts/1-cognito/main.tf
@@ -0,0 +1,5 @@
+resource "aws_cognito_user_pool" "pool" {
+  name = local.system
+
+  alias_attributes = local.alias_attributes
+}
---
+++ infra/aws/parts/1-cognito/variables.tf
@@ -0,0 +1,14 @@
+# リージョン指定
+locals {
+  region    = "us-east-1"
+}
+
+# 環境名
+locals {
+  system = "MENUCARE"
+}
+
+# definition of cognito user pool
+locals {
+  alias_attributes = "email"
+}
