e1601327dcee1c3e6aadec28faef3c6e7f1be796
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-26 19:43:14 +0900

    infra : 1-cognitoへapplication clientを追加

--- infra/aws/parts/1-cognito/main.tf
+++ infra/aws/parts/1-cognito/main.tf
@@ -1,5 +1,13 @@
+// user poolを作成
 resource "aws_cognito_user_pool" "pool" {
   name = local.system
 
   alias_attributes = local.alias_attributes
+}
+
+// application clientを作成
+resource "aws_cognito_user_pool_client" "client" {
+  name = local.system
+
+  user_pool_id = resource.aws_cognito_user_pool.pool
 }
