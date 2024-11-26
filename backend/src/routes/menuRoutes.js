4e13efecc3bbed30d8326f86c74bd05a05171101
Kosuke Honda <kosuke@KosukenoMacBook-Air.local>
2024-11-26 11:33:32 +0900

    add API for admin

--- backend/src/routes/menuRoutes.js
+++ backend/src/routes/menuRoutes.js
@@ -5,5 +5,13 @@ const menuController = require('../controllers/menuController')
 //メニュー一覧の取得
 router.get('/:id/menus', menuController.getMenus);
 
+//メニュー登録
+router.post('/:id/menus', menuController.createMenu);
+
+//メニュー更新
+router.put('/:id/menus/:menu_id', menuController.updateMenu);
+
+//メニュー削除(RESTful)
+router.delete("/:id/menus/:menu_id", menuController.deleteMenu);
 
 module.exports = router;
