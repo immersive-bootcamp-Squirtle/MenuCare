495bd8ace4897344143e04b508c6c3d2599dd315
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-27 10:21:07 +0900

    backend : knexfile.jsを更新

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,15 +1,39 @@
-require('dotenv').config({ path: './.env.local'})
+//ローカル実行するときは以下のコメントアウトを外す
+// require('dotenv').config({ path: './.env.local'})
 
-// process.env
+// // process.env
+
+// module.exports = {
+
+//   development: {
+//     client: 'postgresql',
+//     connection: {
+//       database: process.env.DB_NAME,
+//       user:     process.env.DB_USER,
+//       password: process.env.DB_PASSWORD
+//     },
+//     migrations: {
+//       directory: './db/migrations'
+//     },
+//     seeds: {
+//       directory: './db/seeds'
+//     }
+//   }
+// };
+
+// AWS上へdeployするときは以下のコメントアウトを外す
+require('dotenv').config({ path: './.env.local'})
 
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
+      port: 5432
     },
     migrations: {
       directory: './db/migrations'
