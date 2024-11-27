0b2342e4d5b9a40852dcacb0410d715c7ef10209
ryoto-oguma <0ek5ty4f3263c9e@gmail.com>
2024-11-27 10:51:03 +0900

    kenexfile.jsをローカル実行用に変っこう

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,39 +1,16 @@
 //ローカル実行するときは以下のコメントアウトを外す
-// require('dotenv').config({ path: './.env.local'})
-
-// // process.env
-
-// module.exports = {
-
-//   development: {
-//     client: 'postgresql',
-//     connection: {
-//       database: process.env.DB_NAME,
-//       user:     process.env.DB_USER,
-//       password: process.env.DB_PASSWORD
-//     },
-//     migrations: {
-//       directory: './db/migrations'
-//     },
-//     seeds: {
-//       directory: './db/seeds'
-//     }
-//   }
-// };
-
-// AWS上へdeployするときは以下のコメントアウトを外す
 require('dotenv').config({ path: './.env.local'})
+
+// process.env
 
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
-      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: 5432
+      password: process.env.DB_PASSWORD
     },
     migrations: {
       directory: './db/migrations'
@@ -43,3 +20,26 @@ module.exports = {
     }
   }
 };
+
+// AWS上へdeployするときは以下のコメントアウトを外す
+// require('dotenv').config({ path: './.env.local'})
+
+// module.exports = {
+
+//   development: {
+//     client: 'postgresql',
+//     connection: {
+//       host: process.env.DB_HOST,
+//       database: process.env.DB_NAME,
+//       user:     process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       port: 5432
+//     },
+//     migrations: {
+//       directory: './db/migrations'
+//     },
+//     seeds: {
+//       directory: './db/seeds'
+//     }
+//   }
+// };
